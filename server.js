fa1c3c2a235f6b553c3ae4becccdd27914a24566
JERRY VILLARREAL <nuesion@JERRYs-MacBook-Pro.local>
2017-05-04 11:34:08 -0500

    Add project page

--- server.js
+++ server.js
@@ -40,6 +40,13 @@ app.get('/about', (req, res) => {
   })
 })
 
+app.get('/projects', (req, res) => {
+  res.render('projects.hbs', {
+    pageTitle: 'Projects Page',
+    pageContent: 'All my projects.'
+  })
+})
+
 // /bad
 app.get('/bad', (req, res) => {
   res.send({
